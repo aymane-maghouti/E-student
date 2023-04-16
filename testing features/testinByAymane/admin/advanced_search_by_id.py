537aaf788536430a42652103465bc4198238107b
AymaneMG <aymanemaghouti16@gmail.com>
2023-04-16 15:33:03 +0000

    updated the advanced_search_by_id.py script

--- testing features/testinByAymane/admin/advanced_search_by_id.py
+++ testing features/testinByAymane/admin/advanced_search_by_id.py
@@ -1,5 +1,4 @@
 
-
 import mysql.connector
 from tkinter import *
 from PIL import Image, ImageTk
@@ -14,12 +13,40 @@ mydb = mysql.connector.connect(
 # Créer une instance de la fenêtre Tkinter
 root = Tk()
 
+# Créer une nouvelle fenêtre pour afficher les données
+record_window = Toplevel(root)
 
-# Ajouter des widgets à la fenêtre
+# Créer les frames gauche et droite pour l'affichage des données
+left_frame = Frame(record_window)
+left_frame.pack(side=LEFT, padx=10, pady=10)
+
+right_frame = Frame(record_window)
+right_frame.pack(side=RIGHT, padx=10, pady=10)
+
+# Ajouter des widgets à la frame gauche
 id_label = Label(root, text="ID:")
 id_entry = Entry(root)
 id_label.pack()
 id_entry.pack()
+
+full_name_label = Label(left_frame, text="Full Name:")
+full_name_label.pack()
+full_name_display = Label(left_frame)
+full_name_display.pack()
+
+age_label = Label(left_frame, text="Age:")
+age_label.pack()
+age_display = Label(left_frame)
+age_display.pack()
+
+filier_label = Label(left_frame, text="Filier:")
+filier_label.pack()
+filier_display = Label(left_frame)
+filier_display.pack()
+
+# Ajouter des widgets à la frame droite pour l'affichage de l'image
+image_label = Label(right_frame)
+image_label.pack()
 
 def show_data():
     # Récupérer l'ID entré par l'utilisateur
@@ -34,28 +61,26 @@ def show_data():
     # Récupérer les données de l'enregistrement
     record = mycursor.fetchone()
 
-    # Créer une fenêtre pour afficher les données
-    record_window = Toplevel()
-    full_name_label = Label(record_window, text="Full Name: " + record[1])
-    age_label = Label(record_window, text="Age: " + str(record[2]))
-    filier_label = Label(record_window, text="Filier: " + record[3])
-    full_name_label.pack()
-    age_label.pack()
-    filier_label.pack()
+    # Afficher les données autres que l'image
+    full_name_display.config(text=record[1])
+    age_display.config(text=str(record[2]))
+    filier_display.config(text=record[3])
 
     # Afficher l'image
     image = Image.open(io.BytesIO(record[4]))
     photo = ImageTk.PhotoImage(image)
-    image_label = Label(record_window, image=photo)
+    image_label.config(image=photo)
     image_label.image = photo
-    image_label.pack()
+
+    # Afficher la fenêtre contenant les données
+    record_window.deiconify()
 
 # Ajouter un bouton pour afficher les données
 show_button = Button(root, text="Show Data", command=show_data)
 show_button.pack()
 
+# Masquer la fenêtre record_window au lancement du programme
+record_window.withdraw()
+
 # Afficher la fenêtre Tkinter
 root.mainloop()
-
-
-
